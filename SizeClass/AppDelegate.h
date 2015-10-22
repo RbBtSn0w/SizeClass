600195ac3218cb700c9f62fd7c83de6100f77e17
rbbtsn0w <hamiltonsnow@gmail.com>
2015-10-23 00:21:27 +0800

    init

---
+++ SizeClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SizeClass
+//
+//  Created by Snow Wu on 10/22/15.
+//  Copyright © 2015 Tencent Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClass
+//
+//  Created by Snow Wu on 10/22/15.
+//  Copyright © 2015 Tencent Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
